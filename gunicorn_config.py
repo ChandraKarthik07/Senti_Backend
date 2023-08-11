e7a71121d0721837b1a107f2fbf01257cee53dd2
Satya Sasi Vatsal <71326149+sasivatsal7122@users.noreply.github.com>
2023-08-11 21:37:10 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 # gunicorn_config.py
 
-bind = "127.0.0.1:8000"  # Replace with your desired host and port
+bind = "0.0.0.0:8001"  # Replace with your desired host and port
 workers = 4  # You can adjust this based on your server's capabilities
 threads = 2  # You can also adjust this based on your server's capabilities
 worker_class = "sync"  # You can experiment with other worker classes
