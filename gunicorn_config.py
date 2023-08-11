14db8fa49ff4ec348c295b341e9d1a64cdae1b4f
chandrakarthik07 <chandrakarthik42@gmail.com>
2023-08-11 21:27:56 +0530

    gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,3 +4,4 @@ bind = "127.0.0.1:8000"  # Replace with your desired host and port
 workers = 4  # You can adjust this based on your server's capabilities
 threads = 2  # You can also adjust this based on your server's capabilities
 worker_class = "sync"  # You can experiment with other worker classes
+reload = True
