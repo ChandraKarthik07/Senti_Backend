80055bfc8b152ad8fd169e3776b917f9617d5562
chandrakarthik07 <chandrakarthik42@gmail.com>
2023-08-16 18:56:14 +0530

    .

--- core/serializers.py
+++ core/serializers.py
@@ -30,12 +30,3 @@ class ChannelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class MonthlyStatsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Monthlystats
-        fields = '__all__'
-
-class VideoStatsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Videostats
-        fields = '__all__'
