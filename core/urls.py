b0f931c5d899c3ea7d2863bc8a9144906e22b93d
chandrakarthik07 <chandrakarthik42@gmail.com>
2023-08-17 22:38:26 +0530

    .

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,7 @@ from django.urls import path, include, re_path
 from .views import *
 
 urlpatterns = [
-    path("home/", home, name="test"),
-    path("get_scan_id/", GenerateScanView.as_view(), name="get_scan_id"),
-    path("scan/<str:channel_id>/", ChannelInfoAPIView.as_view(), name="channel-info"),
-    path('signup/', UserSignupView.as_view(), name='signup'),
-    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
 ]
 
 urlpatterns += [
