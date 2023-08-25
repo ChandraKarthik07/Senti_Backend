3a671786de6920d615ee08a9e14da01bb60311cf
chandrakarthik07 <chandrakarthik42@gmail.com>
2023-08-25 13:08:04 +0530

    update gunicorn_config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 # gunicorn_config.py
 
 bind = "0.0.0.0:8002"  # Replace with your desired host and port
-workers = 1  # You can adjust this based on your server's capabilities
-threads = 2  # You can also adjust this based on your server's capabilities
+workers = 2  # You can adjust this based on your server's capabilities
+threads = 3  # You can also adjust this based on your server's capabilities
 worker_class = "sync"  # You can experiment with other worker classes
 reload = True
